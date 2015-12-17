2c00cfec2a9bdbae76c65f7ff6d3782ced232e0a
NojNo <nojan@hotmail.de>
2015-12-17 11:47:51 +0100

    Update tournament.sql

--- tournament.sql
+++ tournament.sql
@@ -19,10 +19,10 @@ DROP TABLE IF EXISTS players;
 create TABLE players(player_id SERIAL PRIMARY KEY, name TEXT);
 -- first check if Players exists, if so, it will be dropped. If not: CREATE TABLE
 DROP TABLE IF EXISTS matches;
-create TABLE matches(winner INTEGER, loser INTEGER, match_id SERIAL Primary KEY);
+create TABLE matches(winner INTEGER REFERENCES players(player_id), loser INTEGER REFERENCES players(player_id), match_id SERIAL Primary KEY);
 -- first check if Matches exists, if so, it will be dropped. If not: CREATE TABLE
 create VIEW number_of_wins AS select players.player_id, players.name, count(matches.winner) AS wins FROM players, matches where players.player_id = matches.winner group by players.player_id;
 
 create VIEW matches_played AS SELECT players.name,  players.player_id, count(*) AS matches FROM players left join matches on players.player_id = matches.winner OR players.player_id = matches.loser group by players.player_id;
 
-create VIEW standings AS SELECT matches_played.player_id, matches_played.name, COALESCE(number_of_wins.wins,0) As wins, matches_played.matches from matches_played left join number_of_wins on matches_played.player_id = number_of_wins.player_id OR wins = 0 Order By wins desc;
+create VIEW standings AS SELECT matches_played.player_id, matches_played.name, COALESCE(number_of_wins.wins,0) As wins, COALESCE(matches_played.matches,0) As matches from matches_played left join number_of_wins on matches_played.player_id = number_of_wins.player_id OR wins = 0 Order By wins desc;
